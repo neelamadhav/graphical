72e0bb41cba0f1ee8d05df6eb42f202d02572821
neelamadhav <neelamadhavg@gmail.com>
2015-11-13 11:34:47 +0530

    changes..

--- ass3/pos.data/expt/run.sh
+++ ass3/pos.data/expt/run.sh
@@ -43,15 +43,6 @@ python format_checker.py temp.pos.input test.pos.feature pos
 python Fscore_pos.py test.pos.feature pos.test
 
 
-echo "All Features"
-python posRun.py
-
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.feature temp.pos.input.feature > temp.pos.feature.pred
-paste -d' ' temp.pos.input temp.pos.feature.pred > test.pos.feature
-
-python format_checker.py temp.pos.input test.pos.feature pos
-python Fscore_pos.py test.pos.feature pos.test
-
 echo "All Features .specialChar.numerical"
 python posRunspecialnum.py
 
