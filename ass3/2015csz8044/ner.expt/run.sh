4af576907c43cca80a33761fa01f5552fe29ca8d
neelamadhav <neelamadhavg@gmail.com>
2015-11-14 09:32:25 +0530

    final assignment

--- ass3/2015csz8044/ner.expt/run.sh
+++ ass3/2015csz8044/ner.expt/run.sh
@@ -2,14 +2,14 @@ cut -d' ' -f1 ner.test | cut -d$'\t' -f1 > temp.ner.input
 
 
 echo "simple"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.simple temp.ner.input > temp.ner.simple.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.simple temp.ner.input > temp.ner.simple.pred
 paste -d' ' temp.ner.input temp.ner.simple.pred > test.ner.simple
 
 python format_checker.py temp.ner.input test.ner.simple ner
 python Fscore_ner.py test.ner.simple ner.test
 
 echo "order1"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.simple.order1 temp.ner.input > temp.ner.simple.order1.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.simple.order1 temp.ner.input > temp.ner.simple.order1.pred
 paste -d' ' temp.ner.input temp.ner.simple.order1.pred > test.ner.simple.order1
 
 python format_checker.py temp.ner.input test.ner.simple.order1 ner
@@ -17,14 +17,14 @@ python Fscore_ner.py test.ner.simple.order1 ner.test
 
 
 echo "order2"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.simple.order2 temp.ner.input > temp.ner.simple.order2.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.simple.order2 temp.ner.input > temp.ner.simple.order2.pred
 paste -d' ' temp.ner.input temp.ner.simple.order2.pred > test.ner.simple.order2
 
 python format_checker.py temp.ner.input test.ner.simple.order2 ner
 python Fscore_ner.py test.ner.simple.order2 ner.test
 
 echo "HMM"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.HMMSimpleTagger --include-input false --model-file ../ner.model.HMM temp.ner.input > temp.ner.hmm.pred
+java -cp $MALLET_INC cc.mallet.fst.HMMSimpleTagger --include-input false --model-file ner.model.HMM temp.ner.input > temp.ner.hmm.pred
 paste -d' ' temp.ner.input temp.ner.hmm.pred > test.ner.hmm
 
 python format_checker.py temp.ner.input test.ner.hmm ner
@@ -34,7 +34,7 @@ python Fscore_ner.py test.ner.hmm ner.test
 echo "All Features"
 python nerRun.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.feature temp.ner.input.feature > temp.ner.feature.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.feature temp.ner.input.feature > temp.ner.feature.pred
 paste -d' ' temp.ner.input temp.ner.feature.pred > test.ner.feature
 
 python format_checker.py temp.ner.input test.ner.feature ner
@@ -44,7 +44,7 @@ python Fscore_ner.py test.ner.feature ner.test
 echo "All Features .specialChar.numerical"
 python nerRunspecialnum.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.feature.specialChar.numerical temp.ner.input.feature > temp.ner.feature.specialChar.numerical.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.feature.specialChar.numerical temp.ner.input.feature > temp.ner.feature.specialChar.numerical.pred
 paste -d' ' temp.ner.input temp.ner.feature.specialChar.numerical.pred > test.ner.feature.specialChar.numerical
 
 python format_checker.py temp.ner.input test.ner.feature.specialChar.numerical ner
@@ -53,7 +53,7 @@ python Fscore_ner.py test.ner.feature.specialChar.numerical ner.test
 echo "All Features .specialChar.numerical.cap"
 python nerRunspecialnumcap.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.feature.specialChar.numerical.cap temp.ner.input.feature > temp.ner.feature.specialChar.numerical.cap.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.feature.specialChar.numerical.cap temp.ner.input.feature > temp.ner.feature.specialChar.numerical.cap.pred
 paste -d' ' temp.ner.input temp.ner.feature.specialChar.numerical.cap.pred > test.ner.feature.specialChar.numerical.cap
 
 python format_checker.py temp.ner.input test.ner.feature.specialChar.numerical.cap ner
@@ -62,7 +62,7 @@ python Fscore_ner.py test.ner.feature.specialChar.numerical.cap ner.test
 echo "All Features .specialChar.numerical.cap.mention.hashtag"
 python nerRunspecialnumcapmention.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.feature.specialChar.numerical.cap.mention.hashtag temp.ner.input.feature > temp.ner.feature.specialChar.numerical.cap.mention.hashtag.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.feature.specialChar.numerical.cap.mention.hashtag temp.ner.input.feature > temp.ner.feature.specialChar.numerical.cap.mention.hashtag.pred
 paste -d' ' temp.ner.input temp.ner.feature.specialChar.numerical.cap.mention.hashtag.pred > test.ner.feature.specialChar.numerical.cap.mention.hashtag
 
 python format_checker.py temp.ner.input test.ner.feature.specialChar.numerical.cap.mention.hashtag ner
@@ -71,7 +71,7 @@ python Fscore_ner.py test.ner.feature.specialChar.numerical.cap.mention.hashtag 
 echo "All Features with pos"
 python nerRunpos.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../ner.model.feature.all.pos temp.ner.input.feature > temp.ner.feature.all.pos.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file ner.model.feature.all.pos temp.ner.input.feature > temp.ner.feature.all.pos.pred
 paste -d' ' temp.ner.input temp.ner.feature.all.pos.pred > test.ner.feature.all.pos
 
 python format_checker.py temp.ner.input test.ner.feature.all.pos ner
--- ass3/2015csz8044/pos.expt/run.sh
+++ ass3/2015csz8044/pos.expt/run.sh
@@ -2,14 +2,14 @@ cut -d' ' -f1 pos.test | cut -d$'\t' -f1 > temp.pos.input
 
 
 echo "simple"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.simple temp.pos.input > temp.pos.simple.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.simple temp.pos.input > temp.pos.simple.pred
 paste -d' ' temp.pos.input temp.pos.simple.pred > test.pos.simple
 
 python format_checker.py temp.pos.input test.pos.simple pos
 python Fscore_pos.py test.pos.simple pos.test
 
 echo "order1"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.simple.order1 temp.pos.input > temp.pos.simple.order1.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.simple.order1 temp.pos.input > temp.pos.simple.order1.pred
 paste -d' ' temp.pos.input temp.pos.simple.order1.pred > test.pos.simple.order1
 
 python format_checker.py temp.pos.input test.pos.simple.order1 pos
@@ -18,7 +18,7 @@ python Fscore_pos.py test.pos.simple.order1 pos.test
 
 <<Com
 echo "order2"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.simple.order2 temp.pos.input > temp.pos.simple.order2.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.simple.order2 temp.pos.input > temp.pos.simple.order2.pred
 paste -d' ' temp.pos.input temp.pos.simple.order2.pred > test.pos.simple.order2
 
 python format_checker.py temp.pos.input test.pos.simple.order2 pos
@@ -26,7 +26,7 @@ python Fscore_pos.py test.pos.simple.order2 pos.test
 Com
 
 echo "HMM"
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.HMMSimpleTagger --include-input false --model-file ../pos.model.hmm temp.pos.input > temp.pos.hmm.pred
+java -cp $MALLET_INC cc.mallet.fst.HMMSimpleTagger --include-input false --model-file pos.model.hmm temp.pos.input > temp.pos.hmm.pred
 paste -d' ' temp.pos.input temp.pos.hmm.pred > test.pos.hmm
 
 python format_checker.py temp.pos.input test.pos.hmm pos
@@ -36,7 +36,7 @@ python Fscore_pos.py test.pos.hmm pos.test
 echo "All Features"
 python posRun.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.feature temp.pos.input.feature > temp.pos.feature.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.feature temp.pos.input.feature > temp.pos.feature.pred
 paste -d' ' temp.pos.input temp.pos.feature.pred > test.pos.feature
 
 python format_checker.py temp.pos.input test.pos.feature pos
@@ -46,7 +46,7 @@ python Fscore_pos.py test.pos.feature pos.test
 echo "All Features .specialChar.numerical"
 python posRunspecialnum.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.feature.specialChar.numerical temp.pos.input.feature > temp.pos.feature.specialChar.numerical.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.feature.specialChar.numerical temp.pos.input.feature > temp.pos.feature.specialChar.numerical.pred
 paste -d' ' temp.pos.input temp.pos.feature.specialChar.numerical.pred > test.pos.feature.specialChar.numerical
 
 python format_checker.py temp.pos.input test.pos.feature.specialChar.numerical pos
@@ -55,7 +55,7 @@ python Fscore_pos.py test.pos.feature.specialChar.numerical pos.test
 echo "All Features .specialChar.numerical.cap"
 python posRunspecialnumcap.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.feature.specialChar.numerical.cap temp.pos.input.feature > temp.pos.feature.specialChar.numerical.cap.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.feature.specialChar.numerical.cap temp.pos.input.feature > temp.pos.feature.specialChar.numerical.cap.pred
 paste -d' ' temp.pos.input temp.pos.feature.specialChar.numerical.cap.pred > test.pos.feature.specialChar.numerical.cap
 
 python format_checker.py temp.pos.input test.pos.feature.specialChar.numerical.cap pos
@@ -64,7 +64,7 @@ python Fscore_pos.py test.pos.feature.specialChar.numerical.cap pos.test
 echo "All Features .specialChar.numerical.cap.mention.hashtag"
 python posRunspecialnumcapmention.py
 
-java -cp "/home/madhav/Desktop/ass3/mallet-2.0.7/class:/home/madhav/Desktop/ass3/mallet-2.0.7/lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --include-input false --model-file ../pos.model.feature.specialChar.numerical.cap.mention.hashtag temp.pos.input.feature > temp.pos.feature.specialChar.numerical.cap.mention.hashtag.pred
+java -cp $MALLET_INC cc.mallet.fst.SimpleTagger --include-input false --model-file pos.model.feature.specialChar.numerical.cap.mention.hashtag temp.pos.input.feature > temp.pos.feature.specialChar.numerical.cap.mention.hashtag.pred
 paste -d' ' temp.pos.input temp.pos.feature.specialChar.numerical.cap.mention.hashtag.pred > test.pos.feature.specialChar.numerical.cap.mention.hashtag
 
 python format_checker.py temp.pos.input test.pos.feature.specialChar.numerical.cap.mention.hashtag pos
